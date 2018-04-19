cdd03ebf8fef5bf895c986c6fd876908a2cc6832
nghia.pham <nghia.pham@huntermacdonald.com>
2018-04-19 14:21:47 +0700

    [demo-200]: Upgrade Gradle
    - Add DockerFile
    - update build script

--- clean-build.sh
+++ clean-build.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 function note() {
     local GREEN NC
     GREEN='\033[0;32m'
@@ -10,18 +9,8 @@ function note() {
 #set -e
 #. ./setup-env.sh
 
-#cd util;                                              note "Building util...";            ./gradlew clean build -x test publishToMavenLocal; cd -
+note "Building discovery server...";       ./gradlew clean build -x test; cd -
 
-cd supporters/sup-discovery-server;            note "Building discovery server...";       ./gradlew clean build -x test; cd -
-cd supporters/sup-config-server;               note "Building config server...";          ./gradlew clean build -x test; cd -
-cd supporters/sup-edge-server;                 note "Building API gateway...";            ./gradlew clean build -x test; cd -
-cd supporters/sup-auth;                        note "Building composite...";              ./gradlew clean build -x test; cd -
-
-cd composite-services/core-composite-service;                 note "Building auth...";     ./gradlew clean build -x test; cd -
-
-cd bus-services/core-product-service;               note "Building product-service...";    ./gradlew clean build -x test; cd -
-cd bus-services/core-user-service;            note "Building user-service...";             ./gradlew clean build -x test; cd -
-
-find . -name *SNAPSHOT.jar -exec du -h {} \;
+find . -name *1.0.jar -exec du -h {} \;
 
 #docker-compose build
