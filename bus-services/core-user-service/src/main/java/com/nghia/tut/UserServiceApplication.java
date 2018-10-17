2bf6d4dfc78abc91382f84629bbd65bd0edc4f8f
nghiapt-vnext <nghiapt@vnext.com.vn>
2018-10-17 16:31:10 +0700

    [USER] adding autoconfig for Email service

--- bus-services/core-user-service/src/main/java/com/nghia/tut/UserServiceApplication.java
+++ bus-services/core-user-service/src/main/java/com/nghia/tut/UserServiceApplication.java
@@ -1,11 +1,14 @@
 package com.nghia.tut;
 
+import com.nghia.libraries.email.infrustructure.mail.EnableEmail;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableEmail
 public class UserServiceApplication {
 
     public static void main(String[] args) {
